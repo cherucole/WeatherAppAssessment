5192e9a6623fc6dac0f12334c6cd0bed9d7c22bf
Cheruiyot Collins <cheruyotcheruyot@gmail.com>
2025-01-10 21:29:52 +0300

    building UI for home view - WIP

--- WeatherAppAssessment/Feature/Home/view/HomeView.swift
+++ WeatherAppAssessment/Feature/Home/view/HomeView.swift
@@ -9,7 +9,50 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            ZStack {
+                Image(.forestSunny)
+                    .resizable()
+//                    .ignoresSafeArea()
+//                    .scaledToFill()
+//                    .frame(maxHeight: .infinity)
+                    .ignoresSafeArea()
+                VStack {
+                    Text("25")
+                    Text("Sunny")
+                }
+                .font(.largeTitle.bold())
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .clipped()
+            
+            VStack {
+                HStack {
+                    VStack {
+                        Text("19")
+                        Text("min")
+                    }
+                    Spacer()
+                    VStack {
+                        Text("19")
+                        Text("min")
+                    }
+                    Spacer()
+                    VStack {
+                        Text("19")
+                        Text("min")
+                    }
+                }
+                .padding(.horizontal)
+                Rectangle()
+                    .fill(.white)
+                    .frame(height: 1)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        }
+        .ignoresSafeArea()
+        .background(Color.sunny)
     }
 }
 
