64ff1ba69cb3b36bdfed25dd63f2fd839031f6da
Cheruiyot Collins <cheruyotcheruyot@gmail.com>
2025-01-11 14:26:59 +0300

    now getting location updates

--- WeatherAppAssessment/Feature/Home/view/HomeView.swift
+++ WeatherAppAssessment/Feature/Home/view/HomeView.swift
@@ -44,7 +44,7 @@ struct HomeView: View {
         .ignoresSafeArea()
         .background(Color.sunny)
         .task {
-//            await weatherVM.getWeather(for: <#T##CLLocationCoordinate2D#>)
+            await weatherVM.getWeather(for: location)
         }
     }
     
